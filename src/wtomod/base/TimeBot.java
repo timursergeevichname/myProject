e7c0932f7aee9ee54b777f54e07335ef0a60fab4
timursergeevich <timursergeevichname@gmail.com>
2021-10-24 13:23:04 +0300

    clear import

--- src/wtomod/base/TimeBot.java
+++ src/wtomod/base/TimeBot.java
@@ -2,10 +2,7 @@ package wtomod.base;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Audio;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.Voice;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
